e49e4a9a52e422a9ee1c1a7ed4b7d7058e2d7c4f
Milomir Babic <redant333@gmail.com>
2023-08-08 20:33:08 +0200

    Add initial module files

---
+++ src/pytest_tuitest/__init__.py
@@ -0,0 +1,3 @@
+"""Pytest plugin for testing TUI(Terminal User Interface) applications."""
+
+__version__ = "0.1.0"
---
+++ src/pytest_tuitest/plugin.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.fixture
+def terminal():
+    return "Add real code here"
