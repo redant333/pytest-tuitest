d0273c70c113b335c4b915ce372574bd03a82d64
Milomir Babic <redant333@gmail.com>
2023-09-17 19:43:46 +0200

    Implement Terminal.get_string_at

--- src/pytest_tuitest/terminal.py
+++ src/pytest_tuitest/terminal.py
@@ -46,6 +46,47 @@ class Terminal:
         self._update_screen()
         return self._screen.buffer[line][column].data
 
+    def get_string_at(self, line: int, column: int, length: int) -> str:
+        """Get the string of given length at the given coordiantes in the terminal.
+
+        This method only supports getting a string that is completely located on
+        one line. For locations without any text, this method uses ' '.
+
+        Args:
+            line (int): Line where the string starts (zero indexed).
+            column (int): Column where the string starts (zero indexed).
+            length (int): Length of the string.
+
+        Raises:
+            OutsideBounds: If the string is not completely located within the
+                terminal bounds.
+
+        Returns:
+            str: The requested string.
+        """
+        # Check if any of the arguments are invalid
+        column_start_outside = column < 0 or column >= self._process.columns
+
+        column_end = column + length - 1
+        column_end_outside = column_end < 0 or column_end >= self._process.columns
+
+        line_outside = line < 0 or line >= self._process.lines
+
+        length_invalid = length <= 0
+
+        if column_start_outside or column_end_outside or line_outside or length_invalid:
+            msg = (f"Requested length {length} at location ({line}, {column})"
+                   " is not valid for terminal size "
+                   f"{self._process.lines}x{self._process.columns}")
+            raise OutsideBounds(msg)
+
+        # All arguments good, retrieve the string
+        self._update_screen()
+        chars = [self._screen.buffer[line][column + offset].data
+                 for offset in range(0, length)]
+
+        return "".join(chars)
+
     def wait_for_output(self) -> None:
         """Block until new output is received from the process."""
         self._process.wait_for_output()
--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -53,3 +53,41 @@ class TestGetCharAt:
         """Verify that an exception is raised for invalid coordinates."""
         with pytest.raises(OutsideBounds):
             terminal.get_char_at(line, column)
+
+
+class TestGetStringAt:
+    """Tests for Terminal.get_string_at."""
+    @pytest.mark.parametrize("terminal", [{"executable": "colors.sh"}], indirect=True)
+    @pytest.mark.parametrize("line, column, length, expected", [
+        (0, 0, 2, "16"),
+        (1, 8, 10, "background"),
+        (5, 0, 80, "Default background | Red foreground" + " " * 45),
+    ])
+    # pylint: disable-next=too-many-arguments
+    def test_returns_expected_value_when_inside_bounds(
+            self, terminal, line, column, length, expected):
+        """Verify that the expected value is returned for valid coordinates."""
+        terminal.wait_for_output()
+        string = terminal.get_string_at(line, column, length)
+
+        msg = f"Got string '{string}', expected '{expected}'"
+        assert string == expected, msg
+
+    @pytest.mark.parametrize("terminal",
+                             [{"executable": "colors.sh", "lines": 10, "columns": 10}],
+                             indirect=True)
+    @pytest.mark.parametrize("line, column, length", [
+        (5, 10, 1),
+        (10, 5, 1),
+        (10, 10, 1),
+        (-1, 5, 1),
+        (5, -1, 1),
+        (-1, -1, 1),
+        (5, 8, 3),
+        (5, 5, -1),
+        (5, 5, 0),
+    ])
+    def test_raises_an_exception_when_outside_bounds(self, terminal, line, column, length):
+        """Verify that an exception is raised for invalid coordinates."""
+        with pytest.raises(OutsideBounds):
+            terminal.get_string_at(line, column, length)
